941418d948d093608cf76bb14e346de32af90b34
Paulus Limma <paulus.limma@gmail.com>
2019-11-30 12:16:37 +0200

    Minor changes to generic multi port adder

--- vhd/generic_multi_port_adder.vhd
+++ vhd/generic_multi_port_adder.vhd
@@ -18,7 +18,7 @@ use ieee.std_logic_1164.all;
 
 entity generic_multi_port_adder is
 	generic(operand_width_g : integer := 16;
-	        n_g             : integer);
+	        n_g             : integer := 2);
 	port(
 		clk, rst_n  : in  std_logic;
 		operands_in : in  std_logic_vector(operand_width_g * (2**n_g) - 1 downto 0);
@@ -32,8 +32,9 @@ architecture structural of generic_multi_port_adder is
 
 	constant num_of_operands_c : integer := 2**n_g;
 
+	-- This array contains cell for each adder input and output.
+	-- Outputs of adders will be also inputs of adders in next layer.
 	type container_type is array (num_of_operands_c * 2 - 1 - 1 downto 0) of std_logic_vector(operand_width_g downto 0);
-
 	signal value_container : container_type;
 
 	component adder is
@@ -46,6 +47,7 @@ architecture structural of generic_multi_port_adder is
 	end component adder;
 
 begin
+	-- This async process initializes the input part of value container.
 	copy_inputs_to_value_container : process(operands_in)
 	begin
 		for i in 0 to num_of_operands_c - 1 loop
@@ -53,7 +55,7 @@ begin
 		end loop;
 	end process;
 
-	generate_adders : for i in 0 to num_of_operands_c - 1 - 1 - 1 generate
+	generate_adders : for i in 0 to num_of_operands_c - 2 generate
 		adder_i : adder
 			generic map(
 				operand_width_g => operand_width_g)
@@ -62,7 +64,7 @@ begin
 				rst_n   => rst_n,
 				a_in    => value_container(i * 2)(operand_width_g - 1 downto 0),
 				b_in    => value_container(i * 2 + 1)(operand_width_g - 1 downto 0),
-				sum_out => value_container(i + num_of_operands_c)
+				sum_out => value_container(num_of_operands_c + i)
 			);
 	end generate;
 
