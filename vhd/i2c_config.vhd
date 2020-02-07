7c9e4b22db6b1c29bac6a9897ca665741e5118b5
Quang Phan <quang.phan@tuni.fi>
2020-02-07 09:26:10 +0200

    Create sclk_gen process

--- vhd/i2c_config.vhd
+++ vhd/i2c_config.vhd
@@ -6,7 +6,7 @@
 -- Author     : Group 21
 -- Company    : 
 -- Created    : 2020-02-05
--- Last update: 2020-02-06
+-- Last update: 2020-02-07
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -42,5 +42,34 @@ end adder;
 -------------------------------------------------------------------------------
 
 architecture rtl of i2c_config is
-  begin
+  constant device_add_c : std_logic_vector(8 - 1 down to 0) := "00110100";
+  constant sclk_half_period_c : integer := 1000000000/i2c_freq_g; 
+  type state_type is (start, data_trans, ack_listening, stop);
+  signal current_state : state_type;
+  signal sclk_r : std_logic;
+  signal sclk_cnt_r : integer range sclk_half_period_c downto 0;
+
+begin
+   sync : process(clk, rst_n)
+   begin
+     if rst_n = '0' then
+     end if;
+     
+   end proess sync;
+     
+   sclk_gen : process(clk, rst_n)
+   begin
+     if rst_n = '0' then
+       sclk_r <= '0';
+       sclk_cnt_r <= 0;
+     elsif clk'EVENT and clk = '1' then
+       if sclk_cnt_r = sclk_half_period_c then
+         sclk_r <= not sclk_r;
+         sclk_cnt_r <= 0;
+       else
+         sclk <= sclk + 1;
+       end if;
+     end if;
+   end process sclk_gen;
+       
 end rtl;
